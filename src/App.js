c48eff30ff866907ae3cc11a6d558e3f996f81e0
otsokeuda <otso.rantala@edu.keuda.fi>
2022-04-29 14:52:44 +0300

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="top">
+        <p className='logo'>VERKKOKAUPPA</p>
+        <input className='search'></input>
+        
+      </div>
+
+      <div className='teksti'>
+        <h2>TUOTTEET</h2>
+      </div>
+
+      <div className='tuotteet'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </div>
+
     </div>
   );
 }
